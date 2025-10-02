b1e33f59fe169065e38cca930b6fe1a9646c1b57
HXY <xinyi.h0105@gmail.com>
2025-10-02 23:05:48 +0200

    update test_simple.py

--- test_simple.py
+++ test_simple.py
@@ -1,13 +1,10 @@
 # Copyright Niantic 2019. Patent Pending. All rights reserved.
 #
-# This software is licensed under the terms of the Monodepth2 licence
-# which allows for non-commercial use only, the full terms of which are made
-# available in the LICENSE file.
+# Licensed under the Monodepth2 licence (for non-commercial use only)
 
 from __future__ import absolute_import, division, print_function
 
 import os
-import sys
 import glob
 import argparse
 import numpy as np
@@ -16,7 +13,7 @@ import matplotlib as mpl
 import matplotlib.cm as cm
 
 import torch
-from torchvision import transforms, datasets
+from torchvision import transforms
 
 import networks
 from layers import disp_to_depth
@@ -26,12 +23,14 @@ from evaluate_depth import STEREO_SCALE_FACTOR
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Simple testing funtion for Monodepthv2 models.')
+        description='Simple testing function for Monodepth2 models.')
 
-    parser.add_argument('--image_path', type=str,
-                        help='path to a test image or folder of images', required=True)
-    parser.add_argument('--model_name', type=str,
-                        help='name of a pretrained model to use',
+    parser.add_argument('--image_path', type=str, required=True,
+                        help='path to a test image or folder of images')
+
+    parser.add_argument('--model_name', type=str, default=None,
+                        help='name of a pretrained model to use '
+                             '(ignore if using --load_weights_folder)',
                         choices=[
                             "mono_640x192",
                             "stereo_640x192",
@@ -41,16 +40,24 @@ def parse_args():
                             "mono+stereo_no_pt_640x192",
                             "mono_1024x320",
                             "stereo_1024x320",
-                            "mono+stereo_1024x320"])
-    parser.add_argument('--ext', type=str,
-                        help='image extension to search for in folder', default="jpg")
-    parser.add_argument("--no_cuda",
-                        help='if set, disables CUDA',
-                        action='store_true')
-    parser.add_argument("--pred_metric_depth",
-                        help='if set, predicts metric depth instead of disparity. (This only '
-                             'makes sense for stereo-trained KITTI models).',
-                        action='store_true')
+                            "mono+stereo_1024x320"
+                        ])
+
+    parser.add_argument('--load_weights_folder', type=str, default=None,
+                        help='path to a folder of model weights (e.g. models/weights_19)')
+
+    parser.add_argument('--num_layers', type=int, default=18,
+                        help='resnet layers (must match training, e.g. 18 or 50)')
+
+    parser.add_argument('--ext', type=str, default="jpg",
+                        help='image extension to search for in folder')
+
+    parser.add_argument("--no_cuda", action='store_true',
+                        help='if set, disables CUDA')
+
+    parser.add_argument("--pred_metric_depth", action='store_true',
+                        help='if set, predicts metric depth instead of disparity. '
+                             '(only makes sense for stereo-trained models)')
 
     return parser.parse_args()
 
@@ -58,54 +65,65 @@ def parse_args():
 def test_simple(args):
     """Function to predict for a single image or folder of images
     """
-    assert args.model_name is not None, \
-        "You must specify the --model_name parameter; see README.md for an example"
-
     if torch.cuda.is_available() and not args.no_cuda:
         device = torch.device("cuda")
     else:
         device = torch.device("cpu")
 
-    if args.pred_metric_depth and "stereo" not in args.model_name:
-        print("Warning: The --pred_metric_depth flag only makes sense for stereo-trained KITTI "
-              "models. For mono-trained models, output depths will not in metric space.")
+    # ==== 加载模型 ====
+    if args.load_weights_folder is not None:
+        # ---- 自己训练的模型 ----
+        print("-> Loading model from custom folder ", args.load_weights_folder)
+        encoder_path = os.path.join(args.load_weights_folder, "encoder.pth")
+        depth_decoder_path = os.path.join(args.load_weights_folder, "depth.pth")
 
-    download_model_if_doesnt_exist(args.model_name)
-    model_path = os.path.join("models", args.model_name)
-    print("-> Loading model from ", model_path)
-    encoder_path = os.path.join(model_path, "encoder.pth")
-    depth_decoder_path = os.path.join(model_path, "depth.pth")
+        # 读取 encoder
+        loaded_dict_enc = torch.load(encoder_path, map_location=device)
+        feed_height = loaded_dict_enc['height']
+        feed_width = loaded_dict_enc['width']
+        encoder = networks.ResnetEncoder(args.num_layers, False)
 
-    # LOADING PRETRAINED MODEL
-    print("   Loading pretrained encoder")
-    encoder = networks.ResnetEncoder(18, False)
-    loaded_dict_enc = torch.load(encoder_path, map_location=device)
+        filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+        encoder.load_state_dict(filtered_dict_enc)
+        encoder.to(device)
+        encoder.eval()
 
-    # extract the height and width of image that this model was trained with
-    feed_height = loaded_dict_enc['height']
-    feed_width = loaded_dict_enc['width']
-    filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
-    encoder.load_state_dict(filtered_dict_enc)
-    encoder.to(device)
-    encoder.eval()
+        # decoder
+        depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc, scales=range(4))
+        loaded_dict = torch.load(depth_decoder_path, map_location=device)
+        depth_decoder.load_state_dict(loaded_dict)
+        depth_decoder.to(device)
+        depth_decoder.eval()
 
-    print("   Loading pretrained decoder")
-    depth_decoder = networks.DepthDecoder(
-        num_ch_enc=encoder.num_ch_enc, scales=range(4))
+    else:
+        # ---- 官方预训练模型 ----
+        assert args.model_name is not None, "Need either --model_name or --load_weights_folder"
+        download_model_if_doesnt_exist(args.model_name)
+        model_path = os.path.join("models", args.model_name)
+        print("-> Loading model from ", model_path)
+        encoder_path = os.path.join(model_path, "encoder.pth")
+        depth_decoder_path = os.path.join(model_path, "depth.pth")
 
-    loaded_dict = torch.load(depth_decoder_path, map_location=device)
-    depth_decoder.load_state_dict(loaded_dict)
+        encoder = networks.ResnetEncoder(18, False)
+        loaded_dict_enc = torch.load(encoder_path, map_location=device)
+        feed_height = loaded_dict_enc['height']
+        feed_width = loaded_dict_enc['width']
+        filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+        encoder.load_state_dict(filtered_dict_enc)
+        encoder.to(device)
+        encoder.eval()
 
-    depth_decoder.to(device)
-    depth_decoder.eval()
+        depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc, scales=range(4))
+        loaded_dict = torch.load(depth_decoder_path, map_location=device)
+        depth_decoder.load_state_dict(loaded_dict)
+        depth_decoder.to(device)
+        depth_decoder.eval()
 
-    # FINDING INPUT IMAGES
+    # ==== 找到要预测的图像 ====
     if os.path.isfile(args.image_path):
-        # Only testing on a single image
         paths = [args.image_path]
         output_directory = os.path.dirname(args.image_path)
     elif os.path.isdir(args.image_path):
-        # Searching folder for images
         paths = glob.glob(os.path.join(args.image_path, '*.{}'.format(args.ext)))
         output_directory = args.image_path
     else:
@@ -113,22 +131,18 @@ def test_simple(args):
 
     print("-> Predicting on {:d} test images".format(len(paths)))
 
-    # PREDICTING ON EACH IMAGE IN TURN
+    # ==== 预测并保存结果 ====
     with torch.no_grad():
         for idx, image_path in enumerate(paths):
 
-            if image_path.endswith("_disp.jpg"):
-                # don't try to predict disparity for a disparity image!
+            if image_path.endswith("_disp.jpg") or image_path.endswith("_disp.jpeg"):
                 continue
 
-            # Load image and preprocess
             input_image = pil.open(image_path).convert('RGB')
             original_width, original_height = input_image.size
             input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
-            input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+            input_image = transforms.ToTensor()(input_image).unsqueeze(0).to(device)
 
-            # PREDICTION
-            input_image = input_image.to(device)
             features = encoder(input_image)
             outputs = depth_decoder(features)
 
@@ -136,9 +150,10 @@ def test_simple(args):
             disp_resized = torch.nn.functional.interpolate(
                 disp, (original_height, original_width), mode="bilinear", align_corners=False)
 
-            # Saving numpy file
+            # 保存 numpy 文件
             output_name = os.path.splitext(os.path.basename(image_path))[0]
             scaled_disp, depth = disp_to_depth(disp, 0.1, 100)
+
             if args.pred_metric_depth:
                 name_dest_npy = os.path.join(output_directory, "{}_depth.npy".format(output_name))
                 metric_depth = STEREO_SCALE_FACTOR * depth.cpu().numpy()
@@ -147,7 +162,7 @@ def test_simple(args):
                 name_dest_npy = os.path.join(output_directory, "{}_disp.npy".format(output_name))
                 np.save(name_dest_npy, scaled_disp.cpu().numpy())
 
-            # Saving colormapped depth image
+            # 保存彩色深度图
             disp_resized_np = disp_resized.squeeze().cpu().numpy()
             vmax = np.percentile(disp_resized_np, 95)
             normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
